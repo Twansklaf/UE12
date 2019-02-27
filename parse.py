4e91f032dafffea251d61cc164b2d44309a3588f
twans <antoine.dolant@u-psud.fr>
2019-02-27 13:23:29 +0100

    created annotation file, made some tools for formatting

--- parse.py
+++ parse.py
@@ -1,6 +1,9 @@
+import sys
 import json
 file = open('twitterdata2.txt', 'r')
 file2= open('twitterdata.txt', 'r')
+file3= open('twitterdata3.txt', 'r')
+
 
 def parse_file(file) :
 	values = {}
@@ -23,10 +26,13 @@ def parse_file(file) :
 
 values = parse_file(file)
 values2 = parse_file(file2)
-
 file.close()		
 file2.close()
+
 values = {**values, **values2}
+values = {**values, **parse_file(file3)}
+
+file3.close()
 
 ids = [key for key in values]
 print(len(ids))
@@ -37,3 +43,21 @@ for key in values :
 	file.write(json.dumps(values[key]) + "\n")
 
 file.close()
+
+def parse_archive() :
+	values = {}
+	file = open('formatted.data', 'r')
+	for line in file.readlines() :
+		# print(line[:-1]) # to remove cr
+		strdump = json.loads(line[:-1])
+		values[strdump['id']] = strdump['text']
+	return values
+
+def create_annot() :
+	file = open("annotation.data","w")
+	for key in dumped :
+		file.write("(" + key + ",_)" + "\n")
+	file.close()
+
+dumped = parse_archive()
+# create_annot()
