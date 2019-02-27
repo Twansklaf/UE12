056ffa1e892feb59a2fa7aa8084a2d8d34a8abf5
twans <antoine.dolant@u-psud.fr>
2019-02-27 12:05:06 +0100

    updated parser

--- parse.py
+++ parse.py
@@ -1,25 +1,39 @@
+import json
 file = open('twitterdata2.txt', 'r')
+file2= open('twitterdata.txt', 'r')
 
-values = {}
-tmpvalue = {}
-for line in file.readlines() :
-	if(len(line.split('id: ')) > 1) :
-		tmpvalue['id'] = line.split('id: ')[1].split("\n")[0]
-	elif (len(line.split('text: ')) > 1) :
-		reading = True
-		tmpvalue['text'] = ""
-		tmpvalue['text'] += line.split('text: ')[1].split('\n')[0]
-	elif (len(line.split('end: ')) > 1) :
-		reading = False
-		values[tmpvalue['id']] = tmpvalue
-		tmpvalue = {}
-	else :
-		if reading :
-			tmpvalue['text'] += line.split('_n')[0]
+def parse_file(file) :
+	values = {}
+	tmpvalue = {}
+	for line in file.readlines() :
+		if(len(line.split('id: ')) > 1) :
+			tmpvalue['id'] = line.split('id: ')[1].split("\n")[0]
+		elif (len(line.split('text: ')) > 1) :
+			reading = True
+			tmpvalue['text'] = ""
+			tmpvalue['text'] += line.split('text: ')[1].split('\n')[0]
+		elif (len(line.split('end: ')) > 1) :
+			reading = False
+			values[tmpvalue['id']] = tmpvalue
+			tmpvalue = {}
+		else :
+			if reading :
+				tmpvalue['text'] += line.split('_n')[0]
+	return values
 
-file.close()
+values = parse_file(file)
+values2 = parse_file(file2)
+
+file.close()		
+file2.close()
+values = {**values, **values2}
 
 ids = [key for key in values]
 print(len(ids))
-print(values['1100706495421992961'])
+# print(values['1100706495421992961'])
 
+file = open('formatted.data', 'w')
+for key in values :
+	file.write(json.dumps(values[key]) + "\n")
+
+file.close()
